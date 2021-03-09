7ba2da250f09c6d179bda8c8b24079b95f467f16
Math1299 <perret.mathieu@bluewin.ch>
2021-03-09 13:38:11 +0100

    MAJ et correction de bugs action vider le panier OK

--- js/cart.js
+++ js/cart.js
@@ -1,20 +1,26 @@
+//Récupération du localStorage en format JSON vers du JS
 const cartItems = JSON.parse(localStorage.getItem("cameras"));
-let x = 0;
-let totalPrice = 0;
+// console.log(cartItems);
 
+//Ajout des données du localStorage tant qu'il y en a
 if (cartItems) {
   for (let i = 0; i < cartItems.length; i++) {
     fetchData(cartItems[i]["id"]);
   }
 }
+
 function fetchData(id) {
   fetch("http://localhost:3000/api/cameras/" + id)
     .then((response) => response.json())
     .then((data) => intoCart(data));
 }
+// Remplissage du panier avec les données récupérées
+
+let nb = 0;
+let totalPrice = 0;
 
 function intoCart(camera) {
-  x = x + 1;
+  nb = nb + 1;
   let table = document.querySelector("table");
   let newLine = document.createElement("tbody");
   let tr = newLine.appendChild(document.createElement("tr"));
@@ -22,17 +28,46 @@ function intoCart(camera) {
   let name = tr.appendChild(document.createElement("td"));
   let price = tr.appendChild(document.createElement("td"));
   let lens = tr.appendChild(document.createElement("td"));
-  //   let supp = tr.appendChild(document.createElement("button"));
-  th.innerHTML = x;
+
+  th.innerHTML = nb;
   name.innerHTML = camera.name;
-  //   supp.className = "btn btn-danger ";
   price.innerHTML = `${camera.price / 100} CHF`;
-  lens.innerHTML = cartItems[x - 1]["lens"];
+  lens.innerHTML = cartItems[nb - 1]["lens"];
 
   table.appendChild(newLine);
+
   totalPrice += camera.price;
-  document.querySelector("h5").innerText = `TOTAL : ${totalPrice} CHF`;
-  //   supp.onclick = function () {
-  //     remove();
-  //   };
+  document.querySelector("h5").innerText = `TOTAL : ${totalPrice / 100} CHF`;
+  // console.log(table);
+  // console.log(tr);
+  // console.log(th);
+  // console.log(name);
+  // console.log(price);
+  // console.log(lens);
+  // console.log(supp);
 }
+
+//-----------------------------------Vider le panier--------------------------------
+
+//Création du bouton a envoyer dans le HTML
+const btnEmptyCart = `<button type="reset"class="btn btn-danger btn-block"id="btnEmptyCart">Vider la panier</button>`;
+
+//Insertion du bouton dans le HTML
+panierContainerRecap.insertAdjacentHTML("beforeend", btnEmptyCart);
+
+const del = document.querySelector("#btnEmptyCart");
+// console.log(del);
+
+//Suppression de la key 'cameras' du localStorage pour vider le panier
+
+del.addEventListener("click", (e) => {
+  e.preventDefault;
+
+  localStorage.removeItem("cameras");
+
+  alert("Le panier va être vidé");
+
+  document.location.reload();
+});
+
+//-------------------------FORMULAIRE-----------------------------------
--- js/index.js
+++ js/index.js
@@ -5,19 +5,27 @@ function fetchData() {
     .then((data) => renderAllCameras(data));
 }
 
+//Fonction recupération des données pour les cameras
 function renderAllCameras(data) {
   for (const camera of data) {
     if ("content" in document.createElement("template")) {
       const main = document.querySelector("#product-list"); //On pointe #product-list
-      let template = document.querySelector("#product"); // On pointe #product
-      let clone = document.importNode(template.content, true); // On assigne les selections
-      let img = clone.querySelector("img");
-      let name = clone.querySelector("p");
-      let button = clone.querySelector("a");
+      const template = document.querySelector("#product"); // On pointe #product
+      const clone = document.importNode(template.content, true); // On assigne les selections
+      const img = clone.querySelector("img");
+      const name = clone.querySelector("p");
+      const button = clone.querySelector("a");
       img.src = camera.imageUrl; // On indique les routes
       button.href = "pages/product.html?id=" + camera._id;
       name.innerHTML = camera.name;
       main.appendChild(clone); // On ajoute toutes les datas à "main"
+      // console.log(camera);
+      // console.log(main);
+      // console.log(template);
+      // console.log(clone);
+      // console.log(img);
+      // console.log(name);
+      // console.log(button);
     }
   }
 }
